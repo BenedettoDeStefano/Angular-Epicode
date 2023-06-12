38f4b8e816f8daa18e6e760637ab458048fcc96a
Benedetto <129066047+BenedettoDeStefano@users.noreply.github.com>
2023-06-12 09:57:50 +0200

    .

--- Settimana2/ProgettoSettimana11/src/app/auth/login/login.component.ts
+++ Settimana2/ProgettoSettimana11/src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { catchError, of } from 'rxjs';
 
 
 @Component({
@@ -19,20 +20,39 @@ export class LoginComponent implements OnInit {
   }
 
 
+accedi(form: NgForm) {
+  console.log(form.value);
 
-  accedi(form: NgForm) {
-    this.isLoading = true;
-    console.log(form.value);
-    try {
-        this.authSrv.login(form.value).subscribe();
-        this.isLoading = false;
-        alert('Login effettuato!');
-        this.router.navigate(['/movies']);
-    } catch (error) {
-        this.isLoading = false;
-        alert('Login sbagliato!');
-        console.error(error);
+  this.authSrv.login(form.value).pipe(
+    catchError(error => {
+      console.error(error);
+      alert('Login Fallito');
+      form.reset();
+      return of(null);
+    })
+  ).subscribe(response => {
+    if (response) {
+      this.router.navigate(['/movies']);
+      alert('Login Effettuato');
     }
+  });
+}
+redirectToRegister() {
+  this.router.navigate(['/register']);
+}
 }
 
-}
+//   accedi(form: NgForm) {
+//     this.isLoading = true;
+//     console.log(form.value);
+//     try {
+//         this.authSrv.login(form.value).subscribe();
+//         this.isLoading = false;
+//         alert('Login effettuato!');
+//         this.router.navigate(['/movies']);
+//     } catch (error) {
+//         this.isLoading = false;
+//         alert('Login sbagliato!');
+//         console.error(error);
+//     }
+// }
